2cc52ad909f7681920dac4881b87a61220072b51
Jaya Kumar <jayakumar1994@gmail.com>
2021-10-19 17:07:26 -0500

    created authentication controller

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using FeaturedDeveloper.Interfaces;
+using FeaturedDeveloper.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeaturedDeveloper.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        private readonly IDeveloperUtility _utility;
+
+        public ILogger<AuthenticationController> _logger { get; }
+
+        public bool IsAdmin;
+
+        public AuthenticationController(IDeveloperUtility _Dutility, ILogger<AuthenticationController> ilogger)
+        {
+            _utility = _Dutility;
+            _logger = ilogger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Create([FromBody] User user)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeaturedDeveloper.Models
+{
+    public class User
+    {
+        [DefaultValue("")]
+        public String UserName { get; set; }
+        [DefaultValue("")]
+        public String Password { get; set; }
+        [DefaultValue("")]
+        public String Email { get; set; }
+    }
+}
